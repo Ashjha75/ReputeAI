9ea3e482cf3a248fbb01ea944f6fff8bc4218292
Ashish jha <ajha5645@gmail.com>
2025-11-12 18:45:02 +0530

    Initialize ReputeAI project with Maven setup and add necessary configuration files

---
+++ server/reputeai/src/main/java/com/reputeai/server/reputeai/ReputeaiApplication.java
@@ -0,0 +1,16 @@
+package com.reputeai.server.reputeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputeaiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReputeaiApplication.class, args);
+
+        System.out.println("✨🪸🍵✨ Reputeai Server Started ✨🪸🍵✨");
+    }
+
+}
---
+++ server/reputeai/src/test/java/com/reputeai/server/reputeai/ReputeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reputeai.server.reputeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputeaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
