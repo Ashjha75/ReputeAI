9ca0d2e56002976e88f361a28f5acd1093bd9b98
Ashish jha <ajha5645@gmail.com>
2025-11-14 15:36:30 +0530

    feat: implement loader component with dynamic configuration and styling

--- frontend/repute-frontend/src/app/app.ts
+++ frontend/repute-frontend/src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 import { Header } from './shared/components/header/header';
+import { LoadingService } from './core/services/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, LoaderComponent, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('repute-frontend');
+  private readonly loadingService = inject(LoadingService);
+  protected readonly loading$ = this.loadingService.loading$;
 }
