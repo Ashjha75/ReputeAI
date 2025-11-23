1eb0d39c58422cee4814c9159b2ccd5082fea5b0
Ashish jha <ajha5645@gmail.com>
2025-11-23 20:28:46 +0530

    refactor: Update Javadoc formatting and improve readability

--- server/reputeai/src/main/java/com/reputeai/server/reputeai/security/JwtProvider.java
+++ server/reputeai/src/main/java/com/reputeai/server/reputeai/security/JwtProvider.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.crypto.SecretKey;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -24,7 +25,6 @@ import java.time.Instant;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
-import javax.crypto.SecretKey;
 
 @Component
 @Slf4j
